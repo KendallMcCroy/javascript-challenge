3ac2a3259bd3b9faab9cf37b5a3ea9ff5314a4b8
Kendall <kdmccroy@gmail.com>
2022-01-17 14:40:34 -0500

    added console.log to check if data loading

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,25 +1,27 @@
-// from data.js
-var tableData = data;
+console.log("app.js loaded")
+
+// // from data.js
+// var tableData = data;
 
 
-// Using the UFO dataset provided in the form of an array of JavaScript 
-// objects, write code that appends a table to your web page and then 
-// adds new rows of data for each UFO sighting.
+// // Using the UFO dataset provided in the form of an array of JavaScript 
+// // objects, write code that appends a table to your web page and then 
+// // adds new rows of data for each UFO sighting.
 
-// Grab the "Filter Table" button
-var filterTableBtn = d3.select("#filter-btn")
+// // Grab the "Filter Table" button
+// var filterTableBtn = d3.select("#filter-btn")
 
-// Select tbody
-var tbody = d3.select("tbody");
+// // Select tbody
+// var tbody = d3.select("tbody");
 
-//Initialize uerDate variable
-var queryDate = 'tbd';
+// //Initialize uerDate variable
+// var queryDate = 'tbd';
 
-// Handle click for the "Filter Table" Button
-filterTableBtn.on("click", function() {
+// // Handle click for the "Filter Table" Button
+// filterTableBtn.on("click", function() {
 
-    // Prevent the page form refreshing 
-    d3.event.preventDefault();
+//     // Prevent the page form refreshing 
+//     d3.event.preventDefault();
 
 //     // Select input element and get HTML node
 //     var inputField = d3.select("#datetime");
@@ -29,20 +31,23 @@ filterTableBtn.on("click", function() {
 
 
 //     //Create filtered Data from data table
-//     var filtered_Data = tableData.filter(function (sighting) {
+//     var filteredData = tableData.filter(function (sighting) {
 //         return sighting.datetime === queryDate;
-//     })
+//     });
 
-// // Clear the data in tbody
-// tbody.text('');
+//     // Clear the data in tbody
+//     tbody.text('');
 
-//     filtered_Data.forEach(record => {
+//     // Populate tbody with rows and cells needed for filterdData
+//     filteredData.forEach(record => {
 
-
+//         // Assign new appended row into a variable
 //         var row = tbody.aqqend('tr');
 
-//         Object.entries(record).forEach(([key, value]) => {
+//         // Loop throught records
+//             Object.entries(record).forEach( ([key, value]) => {
 
+//             // Append a "td" element to the row with the value of each key in the object
 //             row.append('td').text(value);
 
 //         });
