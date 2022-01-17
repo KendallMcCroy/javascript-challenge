1357b00c626a723edca8da89b9fdeac3986f2f77
Kendall <kdmccroy@gmail.com>
2022-01-16 21:49:51 -0500

    created app.js page

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
+
+// Using the UFO dataset provided in the form of an array of JavaScript 
+// objects, write code that appends a table to your web page and then 
+// adds new rows of data for each UFO sighting.
+
+// Grab the "Filter Table" button
+var filtertablebutton = d3.select("#filter-btn")
+
+
 // YOUR CODE HERE!
